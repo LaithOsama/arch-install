f3496b23942fda09e89cad6c16f11c94afdf3e0b
Laith Osama <81252120+LaithOsama@users.noreply.github.com>
2021-12-25 07:39:16 +0300

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -8,24 +8,22 @@ echo "/swapfile   none  swap  defaults  0 0" >> /etc/fstab
 ln -sf /usr/share/zoneinfo/Asia/Baghdad /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "ar_IQ.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "KEYMAP=us" > /etc/vconsole.conf
 echo "laptop" > /etc/hostname
+mkinitcpio -P
 passwd
 pacman --noconfirm -S grub ntfs-3g os-prober
 grub-install --target=i386-pc /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
 echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
-
-pacman -S --noconfirm xorg-server xorg-xinit xorg-xsetroot xorg-xrandr \
-xwallpaper sxiv zathura zathura-pdf-mupdf mpd \
-mpv cmus neovim newsboat unrar unzip wget \
-zsh wpa_supplicant connman kodi xcompmgr transmission-gtk \
-man-db htop alsa-utils libnotify dunst dash
-
-systemctl enable connmanctl wpa_supplicant
+grub-mkconfig -o /boot/grub/grub.cfg
+pacman -Sy --noconfirm xorg-server xorg-xinit xorg-xsetroot xorg-xrandr \
+xwallpaper sxiv zathura zathura-pdf-mupdf stow maim \
+mpv cmus neovim newsboat unrar unzip wget pcmanfm calcurse \
+zsh wpa_supplicant dhcpcd kodi xcompmgr transmission-gtk \
+man-db htop alsa-utils libnotify dunst dash arc-gtk-theme
+systemctl enable dhcpcd wpa_supplicant
 rm /bin/sh
 ln -s dash /bin/sh
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
